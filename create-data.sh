f9a78d210668726b82009f295dff556846b833f3
Vikrom Narula <vikrom.nar@gmail.com>
2022-03-27 15:17:08 +0700

    add copy data

--- create-data.sh
+++ create-data.sh
@@ -6,3 +6,7 @@ mkdir database-data
 rm -rf database-data/*
 
 ./tpcc-generator 10 database-data
+
+cp -r database-data ../../apis/js-thesis-project
+cp -r database-data ../../apis/python-thesis-project
+cp -r database-data ../../apis/rust-thesis-project
